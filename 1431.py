f93b61bb41829debc2599af975b15023dcb7276c
Marcusvon <marcusvon@algartelecom.com.br>
2024-09-03 23:18:25 -0300

    Subindo exercicio python

--- 1431.py
+++ 1431.py
@@ -1,13 +1,19 @@
+from typing import List
+
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        def verifica_igualdade(candidate: str, s: str) -> bool:
-            repetitions = len(s) // len(candidate)
-            if candidate * repetitions == s:
-                return True
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxV = candies[0] + extraCandies
+        output = []
+        for i in candies:
+            if(i + extraCandies >= maxV):
+                output.append(True)
+            else:
+                output.append(False')
+        return(output)
 
-        for i in range(min(len(str1), len(str2)), 0, -1):
-            candidate = str1[:i]
-            if verifica_igualdade(candidate, str1) and verifica_igualdade(candidate, str2):
-                return(candidate)
-
-        return ""
+# Testando a função com os parâmetros fornecidos
+candies = [2,3,5,1,3]
+extraCandies = 3
+solution = Solution()
+result = solution.kidsWithCandies(candies, extraCandies)
+print(result)
--- 1768.py
+++ 1768.py
@@ -1,13 +1,22 @@
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        def verifica_igualdade(candidate: str, s: str) -> bool:
-            repetitions = len(s) // len(candidate)
-            if candidate * repetitions == s:
-                return True
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = []
+        i = 0
+        len1, len2 = len(word1), len(word2)
 
-        for i in range(min(len(str1), len(str2)), 0, -1):
-            candidate = str1[:i]
-            if verifica_igualdade(candidate, str1) and verifica_igualdade(candidate, str2):
-                return(candidate)
+        # Alternar entre as letras de word1 e word2
+        while i < len1 and i < len2:
+            result.append(word1[i])
+            result.append(word2[i])
+            i += 1
+        
+        # Adicionar as letras restantes de word1, se houver
+        if i < len1:
+            result.append(word1[i:])
 
-        return ""
+        # Adicionar as letras restantes de word2, se houver
+        if i < len2:
+            result.append(word2[i:])
+
+        # Unir a lista em uma string e retornar
+        return ''.join(result)
